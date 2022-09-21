fcc1505038ea03aff77a38ef4c3781db5b5472b9
jackruder <jackruder14@gmail.com>
2022-09-21 09:04:38 -0700

    sync

--- Graphics/hw2/TransformCalculator.java
+++ Graphics/hw2/TransformCalculator.java
@@ -3,9 +3,10 @@
  * @date Sep 12, 2022
  * */
 
-import org.joml.Matrix4d;
-import org.joml.Vector3d;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import java.util.Scanner;
+import java.util.Vector;
 
 /**
  * Class to compute transformations of points in 3D space
@@ -22,7 +23,7 @@ public class TransformCalculator {
 	 */
 	public static void main(String[] args) {
 
-		Matrix4d matrix = new Matrix4d(); // initialize identity matrix
+		Matrix4f matrix = new Matrix4f(); // initialize identity matrix
 		Scanner s = new Scanner(System.in);
 		boolean exit = false;
 
@@ -54,7 +55,7 @@ public class TransformCalculator {
 				break;
 			}
 
-			Vector3d v;
+			Vector3f v;
 			try {
 				// get point
 				v = parsePoint(input.trim());
@@ -71,36 +72,36 @@ public class TransformCalculator {
 	}
 
 	/**
-	 * Parse a string into a Vector3d object
+	 * Parse a string into a Vector3f object
 	 * 
 	 * @param s
 	 *        string to parse
 	 * @exception IllegalArgumentException
 	 *            if string cannot be parsed
-	 * @return Vector3d object
+	 * @return Vector3f object
 	 */
-	public static Vector3d parsePoint(String s) {
+	public static Vector3f parsePoint(String s) {
 		String[] sargs = s.split("\\s+");
 		if (sargs.length != 3) { // check length
 			throw new IllegalArgumentException("Could not parse point: expected 3 points, got "
 					+ Integer.toString(sargs.length));
 		} else {
-			double[] args = new double[sargs.length];
+			float[] args = new float[sargs.length];
 
-			try { // ensure all arguments after first are doubles
+			try { // ensure all arguments after first are floats
 				for (int i = 0; i < args.length; i++) {
-					args[i] = Double.parseDouble(sargs[i]); // make casts
+					args[i] = Float.parseFloat(sargs[i]); // make casts
 				}
 			} catch (Exception e) {
 				throw new IllegalArgumentException("Could not parse point: non-numerical arguments");
 			}
 
-			return new Vector3d(args[0], args[1], args[2]);
+			return new Vector3f(args[0], args[1], args[2]);
 		}
 	}
 
 	/**
-	 * Applies a transformation to a Matrix4d object as described by a given string
+	 * Applies a transformation to a Matrix4f object as described by a given string
 	 * 
 	 * @param m
 	 *        matrix to modify
@@ -110,16 +111,16 @@ public class TransformCalculator {
 	 *            if string cannot be parsed
 	 * @return modified matrix
 	 */
-	public static Matrix4d stringTransform(String s, Matrix4d m) throws IllegalArgumentException {
+	public static Matrix4f stringTransform(String s, Matrix4f m) throws IllegalArgumentException {
 		String[] sargs = s.split("\\s+"); // split whitespace
 
 		if (sargs.length < 2) { // too few arguments
 			throw new IllegalArgumentException("Could not parse transformation: no arguments provided");
 		}
-		double[] args = new double[sargs.length - 1]; // skip transform id
-		try { // ensure all arguments after first are doubles
+		float[] args = new float[sargs.length - 1]; // skip transform id
+		try { // ensure all arguments after first are floats
 			for (int i = 0; i < args.length; i++) {
-				args[i] = Double.parseDouble(sargs[i + 1]);
+				args[i] = Float.parseFloat(sargs[i + 1]);
 			}
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Could not parse transformation: non-numerical arguments");
@@ -154,8 +155,8 @@ public class TransformCalculator {
 			// Rotation syntax expects degrees, JOML requires radians
 			// Have to convert first
 		case "RX":
-			if (args.length == 1) {
-				double rad = java.lang.Math.toRadians(args[0]);
+			if (args.length == 1) { //theta
+				float rad = (float)java.lang.Math.toRadians(args[0]);
 				return m.rotateX(rad);
 			} else {
 				invalid = 1;
@@ -163,8 +164,8 @@ public class TransformCalculator {
 			break;
 
 		case "RY":
-			if (args.length == 1) {
-				double rad = java.lang.Math.toRadians(args[0]);
+			if (args.length == 1) { //theta
+				float rad = (float)java.lang.Math.toRadians(args[0]);
 				return m.rotateY(rad);
 			} else {
 				invalid = 1;
@@ -172,8 +173,8 @@ public class TransformCalculator {
 			break;
 
 		case "RZ":
-			if (args.length == 1) {
-				double rad = java.lang.Math.toRadians(args[0]);
+			if (args.length == 1) { //theta
+				float rad = (float)java.lang.Math.toRadians(args[0]);
 				return m.rotateZ(rad);
 			} else {
 				invalid = 1;
@@ -182,8 +183,30 @@ public class TransformCalculator {
 
 		// look at
 		case "L":
-			if (args.length == 6) {
-				return m.lookAlong(args[0], args[1], args[2], args[3], args[4], args[5]); // look at
+			if (args.length == 6) { // cx, cy, cz, tx, ty, tz
+				float cx = args[0];
+				float cy = args[1];
+				float cz = args[2];
+				float tx = args[3];
+				float ty = args[4];
+				float tz = args[5];
+	
+				Vector3f f = new Vector3f(tx-cx, ty-cy, tz-cz).normalize(); // forwards vector (vec pointing in -Z direction)
+
+				if (f.equals(0,1,0) || f.equals(0,-1,0)) {
+					throw new IllegalArgumentException("Could not parse transformation: cannot look directly up or down");
+				}
+	
+				// r = ||f x y||
+				Vector3f r = new Vector3f(); //TODO handle f = +-y
+				f.cross(0,1,0,r); 
+				r.normalize();
+
+				// u = r x f
+				Vector3f u = new Vector3f();
+				r.cross(f,u);
+				
+				return m.lookAlong(f.negate(), u); // look at
 														// (0,0,0)
 			} else {
 				invalid = 6;
@@ -193,7 +216,7 @@ public class TransformCalculator {
 		// orthogonal projection
 		case "O":
 
-			if (args.length == 4) {
+			if (args.length == 4) { //w, h, dn, df
 				return m.orthoSymmetric(args[0], args[1], args[2], args[3]);
 			} else {
 				invalid = 4;
@@ -202,8 +225,10 @@ public class TransformCalculator {
 
 		// perspective shift
 		case "P":
-			if (args.length == 4) {
-				return m.perspective(args[0], args[1], args[2], args[3]);
+			if (args.length == 4) { //horizontal fov, aspect ratio, dn, df
+				float fovx = (float)java.lang.Math.toRadians(args[0]);
+				float fovy = (float)(2 * java.lang.Math.atan(java.lang.Math.tan(fovx/2) / args[1])); //convert horizontal to vertical using trig
+				return m.perspective(fovy, args[1], args[2], args[3]);
 			} else {
 				invalid = 4;
 			}
@@ -217,7 +242,4 @@ public class TransformCalculator {
 				+ Integer.toString(invalid) + " arguments, got " + Integer.toString(args.length));
 	}
 
-	/** prevent instantiation */
-	protected TransformCalculator() {
-	}
 }
---
+++ Stats/warmups/polarprelab/ppl.r
@@ -0,0 +1,121 @@
+##########################f
+# Author: Jack Ruder
+# Date: September 18, 2022 
+# Polar Pre Lab
+###########################
+library(MASS)
+# Data location constants
+DATA_DIR <- "~/School/Stats/data" # can change to . for local
+#DATA_DIR <- "." # can change to . for local
+iceCorefile <- "ice_core.csv"
+isoTfile <- "isotope_and_T.csv"
+fsSep <- "/" # posix
+
+#load data
+iceCore <- read.csv(paste(DATA_DIR,iceCorefile, sep=fsSep))
+isoT <- read.csv(paste(DATA_DIR, isoTfile, sep=fsSep))
+
+
+
+#Question 1.
+plot(isoT$delta18O, isoT$temperature)
+# The plot exhibits some approximately linear behavior (positive relationship), although there is a potential slight bend upwards and an outlier.
+
+
+# There is a clear outlier, although it is hard to determine that it is a bad datapoint since it is within the range of data. 
+
+subset(isoT, X==82) # the outlier, at y=-5.4 degrees.
+# in the temperature_corrected column, there is an entry of -55.4,
+# it appears that the user forgot to type a 5.
+
+# We confirm this,
+subset(isoT, X > 78 & X < 86)$temperature
+# we see that indeed -55.4 is in line with all of the surrounding X values, so this corrected mistake appears to be appropriate
+
+#for readibility, set temp column to temperature_corrected
+isoT$temperature <- isoT$temperature_corrected
+
+
+### Diagnostic plotting function #########
+plot.lm <- function(x, y, xlab, ylab) {
+	par(mfrow=c(2,2))
+	plot(x, y, xlab=xlab, ylab=ylab)
+	model <- lm(y~x)
+	abline(model)
+	summary(model)
+
+	#diagnostic plots
+	plot(model$fitted, model$resid)
+	hist(model$resid)
+	# Slight left skew in the residuals
+	qqnorm(model$resid)
+	qqline(model$resid)
+}
+
+
+
+######### fit data as is############
+x <- isoT$delta18O
+y <- isoT$temperature
+
+# scatter plot with fitted line
+plot.lm(x,y,"delta18O", "temperature")
+
+#diagnostic plots
+
+# We see that the data here is centered around 0, however there appears to be 
+# a pattern in the residuals for lower values, with increased spread
+
+# Slight left skew in the residual histogram
+
+#QQ plot here has long tails on the edges. This data should need a transformation
+
+hist(y, breaks=20)
+hist(x, breaks=20)
+# it seems that there are higher frequencies of datapoints at the extremes, we can try some transformations, possibly a square root transform
+
+hist(log(-1/y), breaks=20)
+hist(1/x, breaks=20)
+
+######### try square root transforms data as is############
+plot.lm(sqrt(-x), sqrt(-y), xlab="sqrt(-delta18O)", ylab="sqrt(-ylab)")
+
+#diagnostic plots
+# We see that the data here is centered around 0, however there appears to be 
+# a pattern in the residuals for lower values, with increased spread. The residuals look less normal and more skewed.
+############### not too helpful. clearly, this plot is no better ###############
+plot.lm(sqrt(-x), sqrt(-y), xlab="sqrt(-delta18O)", ylab="sqrt(-ylab)")
+
+
+plot.lm(sqrt(1/-x), sqrt(1/-y), xlab="sqrt(-delta18O)", ylab="sqrt(-ylab)")
+
+
+px <- -x
+py <- -y
+# box cox transform
+bc <- boxcox(py ~ px)
+lambda <- (bc$x)[which.max(bc$y)]
+#fit new linear regression model using the Box-Cox transformation
+ty <- ((py^lambda-1)/lambda)
+plot(ty ~ px)
+bcmodel <- lm(ty ~ px)
+abline(bcmodel)
+
+#evaluate fit
+plot(bcmodel$fitted, bcmodel$resid)
+# Here the data is scattered much better in the plot, although there is some clumping around the fitted values of 100.
+hist(bcmodel$resid)
+# 
+# Slight left skew in the residuals
+qqnorm(bcmodel$resid)
+qqline(bcmodel$resid)
+#QQ plot here has long tails on the edges. This data should need a transformation
+
+
+
+
+
+
+
+
+
