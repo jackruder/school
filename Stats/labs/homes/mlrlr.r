8430b301371a2f065a8562b45e4544519613b12d
jackruder <jackruder14@gmail.com>
2022-10-12 10:16:24 -0700

    up

--- Stats/labs/homes/mlrlr.r
+++ Stats/labs/homes/mlrlr.r
@@ -1,29 +1,14 @@
 ################
 # Author: Jack Ruder
 # Date: Oct 06, 2022
-# Multiple Linear Regression Pre-Lab
+# Multiple Linear Regression Lab report III
 ###############################
-#Warmup answers
-#
-# Variable		Description
-#Distance		Distance travelled in feet
-#Hang		Time in air in seconds
-#R_Strength		Right leg strength in pounds
-#L_Strength		Left leg strength in pounds
-#R_Flexibility		Right leg flexibility in degrees
-#L_Flexibility		Left leg flexibility in degrees
-#O_Strength		Overall leg strength in pounds
-#
-# Obviously, distance and left/right strength (with an indicator term) are going to be the most useful.
-# One of these models would be helpful in identifying weaknesses in someone's performance, to identify skill vs. strength, or to weed out certain players based on their metrics.
-################
-
 DATADIR <- "~/School/Stats/data"
-FILENAME <- "punting.txt"
+FILENAME <- "kc_house_data.csv"
 SEP <- "/" # posix
 
 DATAFILE <- paste(DATADIR, FILENAME, sep=SEP)
-df <- read.table(DATAFILE,header=TRUE)
+df <- read.csv(DATAFILE)
 #attach(df)
 
 library(ggplot2)
@@ -104,11 +89,3 @@ fname <- gsub(" ", "_", colname)
 png(paste(fname,"png",sep="."), width=RES_X, height=RES_Y)
 plotdensity(x, df, colname)
 dev.off()
-
-
-#observe that all of the variables appear to be fairly normally distributed, no obvious tronsformations are necessary
-
-m <- lm(Distance~L_Strength + R_Strength + R_Flexibility +  O_Strength, data=df)
-# adjusted r squared of 0.943, not a terrible overfit
-summary(m)
-anova(m)
