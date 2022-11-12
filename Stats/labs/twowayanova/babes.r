23a053f182abefbb40adc0d224b2326348b270ea
jackruder <jackruder14@gmail.com>
2022-11-12 12:30:47 -0800

    lab done

--- Stats/labs/twowayanova/babes.r
+++ Stats/labs/twowayanova/babes.r
@@ -1,4 +1,4 @@
-
+#################################
 library(knitr)
 library(ggplot2)
 library(hrbrthemes)
@@ -16,26 +16,26 @@ attach(babes)
 (cellCt <- tapply(HatchBmass, list(egg.trmt, inc.envmt), length)) # not balanced
 #kable(data.frame(cellCt), "latex")
 
-svglite("int.svg",width=8,height=6)
+png("int.png",width=8,height=6, units='in', res=150)
 par(mfrow=c(1,2))
 interaction.plot(inc.envmt,as.factor(egg.trmt),HatchBmass)
 interaction.plot(egg.trmt,as.factor(inc.envmt),HatchBmass)
 dev.off()
 #mostly parallel, but not perfect
 
-svglite("bp1.svg",width=8,height=6)
+png("bp1.png",width=8,height=6, units='in', res=150)
 ggplot(babes, aes(x=interaction(inc.envmt,egg.trmt),y=HatchBmass,color=inc.envmt))+
 	geom_violin(alpha=0.2) +
 	geom_point() + 
 	geom_boxplot(alpha=0.1, size=0.2) + 
 	theme_ipsum()
 dev.off()
-svglite("bp2.svg",width=8,height=6)
+png("bp2.png",width=8,height=6, units='in', res=150)
 ggplot(babes, aes(x=interaction(egg.trmt, inc.envmt),y=HatchBmass, color=egg.trmt))+
 	geom_violin(alpha=0.2) +
 	geom_point() + 
 	geom_boxplot(alpha=0.1, size=0.2) + 
-	theme_ipsum()
+	theme_ipsum()e
 dev.off()
 
 
@@ -61,12 +61,13 @@ babes <- babes %>%
 		)
 anova(an)
 
-svglite("inference.svg",width=8,height=6)
+png("inference.png",width=8,height=6, units='in', res=150)
 par(mfrow=c(2,2))
 plot(an,1)
 plot(an,2)
 comp <- babes$trmtEffect * babes$envmtEffect / grand
 plot(an$resid~comp, main="Tukey Non-Additivity Plot")
+abline(lm(an$resid~comp))
 
 cellSD <- as.vector(tapply(HatchBmass, list(inc.envmt,egg.trmt), sd))
 cellM <- as.vector(tapply(HatchBmass, list(inc.envmt,egg.trmt), mean))
@@ -75,3 +76,6 @@ abline((a <- lm(log(cellSD)~log(cellM))))
 dev.off()
 summary(a) # slope 1.162, looks good
 ##################
+sdErr <- anova(an)[3,3]
+(trmtEffectSize <- TukeyHSD(an)$egg.trmt[1] / sdErr)
+(envrEffectSize <- TukeyHSD(an)$inc.envmt[1] / sdErr)
