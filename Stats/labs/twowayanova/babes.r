584c84116f06fb43d681e595d0f90abe092879ca
jackruder <jackruder14@gmail.com>
2022-11-11 19:31:04 -0800

    main stuff done

--- Stats/labs/twowayanova/babes.r
+++ Stats/labs/twowayanova/babes.r
@@ -40,30 +40,38 @@ dev.off()
 
 
 
-
-##############
+###########################
+# non-transformed analysis
+###########################
 (grand <- mean(HatchBmass))
 (trmtEffect <- tapply(HatchBmass, egg.trmt, mean) - grand)
 (envmtEffect <- tapply(HatchBmass, inc.envmt, mean) - grand)
 (an <- aov(HatchBmass~egg.trmt + inc.envmt))
 babes <- babes %>%
-	groupBy(egg.trmt) %>%
+	group_by(egg.trmt) %>%
 		mutate(
 		      trmtEffect = mean(HatchBmass) - grand
 		)
 
 babes <- babes %>%
-	groupBy(inc.envmt) %>%
+	group_by(inc.envmt) %>%
 		mutate(
 		      envmtEffect = mean(HatchBmass) - grand,
 		      res = HatchBmass - trmtEffect - envmtEffect
 		)
 anova(an)
-par(mfrow=c(1,3))
+
+svglite("inference.svg",width=8,height=6)
+par(mfrow=c(2,2))
 plot(an,1)
 plot(an,2)
+comp <- babes$trmtEffect * babes$envmtEffect / grand
+plot(an$resid~comp, main="Tukey Non-Additivity Plot")
+
 cellSD <- as.vector(tapply(HatchBmass, list(inc.envmt,egg.trmt), sd))
 cellM <- as.vector(tapply(HatchBmass, list(inc.envmt,egg.trmt), mean))
 plot(log(cellSD)~log(cellM))
 abline((a <- lm(log(cellSD)~log(cellM))))
-summary(a) # slope 1.162, we should transform to log
+dev.off()
+summary(a) # slope 1.162, looks good
+##################
